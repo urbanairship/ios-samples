8e1f2467128c452f9a4a537b7edcd47a43b4c3db
Jimmy Dee <jimmy@dee.name>
2011-09-27 17:53:52 -0500

    First cut of a split view app skeleton for a tutorial.

---
+++ SplitViewInboxSample/SplitViewInboxSample/SplitViewInboxSampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SplitViewInboxSampleAppDelegate.h
+//  SplitViewInboxSample
+//
+//  Created by Jimmy Dee on 9/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplitViewInboxSampleAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ SplitViewInboxSample/SplitViewInboxSample/UAInboxSplitUI.m
@@ -0,0 +1,94 @@
+//
+//  UAInboxSplitUI.m
+//  SplitViewInboxSample
+//
+//  Created by Jimmy Dee on 9/27/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UAInboxSplitUI.h"
+#import "UAInboxMessageList.h"
+#import "UAInboxMessageListController.h"
+#import "UAInboxMessageViewController.h"
+#import "UAInboxUI.h"
+
+@interface UAInboxSplitUI ()
+
+@property (nonatomic, retain) UAInboxAlertHandler *alertHandler;
+@property (nonatomic, retain) UAInboxMessageListController* mlc;
+@property (nonatomic, retain) UAInboxMessageViewController* mvc;
+@property (nonatomic, assign) BOOL isVisible;
+
+@end
+
+@implementation UAInboxSplitUI
+@synthesize useOverlay;
+@synthesize localizationBundle;
+@synthesize splitViewController;
+@synthesize alertHandler;
+@synthesize isVisible;
+@synthesize mlc;
+@synthesize mvc;
+
+SINGLETON_IMPLEMENTATION(UAInboxSplitUI);
+
+- (void)dealloc
+{
+    RELEASE_SAFELY(localizationBundle);
+    RELEASE_SAFELY(alertHandler);
+    RELEASE_SAFELY(splitViewController);
+    RELEASE_SAFELY(mvc);
+    RELEASE_SAFELY(mlc);
+    [super dealloc];
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        /*
+         * TODO: Setup split view and other initialization
+         */
+    }
+    
+    return self;
+}
+
+- (void)quitInbox
+{
+    /*
+     * TODO: Implement quitInbox
+     */
+    
+}
+
++ (void)quitInbox
+{
+    [[UAInboxSplitUI shared] quitInbox];
+}
+
++ (void)loadLaunchMessage
+{
+    /*
+     * TODO: Display a launch message, if any
+     */
+    
+}
+
++ (void)displayInbox:(UIViewController *)viewController animated:(BOOL)animated
+{
+    /*
+     * TODO: Display the inbox view controller
+     */
+   
+}
+
++ (void)displayMessage:(UIViewController *)viewController message:(NSString *)messageID
+{
+    /*
+     * TODO: Display the requested message
+     */
+    
+}
+
+@end
