04835fcf64a6b4d63a17f7a74bd5df4884a0c981
Jimmy Dee <jimmy@dee.name>
2011-10-04 18:45:01 -0500

    example using a dictionary

--- TabBarInboxSample/TabBarInboxSample/JSDelegate.m
+++ TabBarInboxSample/TabBarInboxSample/JSDelegate.m
@@ -11,10 +11,43 @@
 @implementation JSDelegate
 
 - (NSString*)callbackArguments:(NSArray *)args withOptions:(NSDictionary *)options
-{
+{    
     NSString* varName = [args objectAtIndex:0];
-    NSString* deviceName = [[UIDevice currentDevice] name];
-    return [NSString stringWithFormat:@"%@.deviceName = \"%@\"; %@.finishedLoading();", varName, deviceName, varName];
+    if (varName == nil) {
+        UALOG(@"no variable name provided, can't respond to user");
+        return nil;
+    }
+    
+    NSString* fields = [options valueForKey:@"fields"];
+    if (fields == nil) {
+        return [NSString stringWithFormat:@"%@.error = 'missing fields parameter'; %@.finishedLoading();", varName, varName];
+    }
+    
+    bool error = false;
+    NSArray* fieldList = [fields componentsSeparatedByString:@","];
+    
+    NSString* response = [NSString string];
+    UIDevice* device = [UIDevice currentDevice];
+    
+    for (int j=0; !error && j<fieldList.count; ++j) {
+        NSString* field = [fieldList objectAtIndex:j];
+        if ([field isEqualToString:@"deviceName"]) {
+            response = [response stringByAppendingFormat:@"%@.deviceName = \"%@\";", varName, [device name]];
+        }
+        else if ([field isEqualToString:@"systemVersion"]) {
+            response = [response stringByAppendingFormat:@"%@.systemVersion = \"%@\";", varName, [device systemVersion]];
+        }
+        else {
+            error = true;
+        }
+    }
+    
+    if (!error) {
+        return [response stringByAppendingFormat:@"%@.finishedLoading();", varName];
+    }
+    else {
+        return [NSString stringWithFormat:@"%@.error = 'invalid field'; %@.finishedLoading();", varName, varName];
+    }
 }
 
 @end
